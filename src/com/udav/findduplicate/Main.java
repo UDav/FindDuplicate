28fbd60da000807c607a280d4142458952167ca9
UDav <iadavidov89@gmail.com>
2012-12-11 18:29:44 +0600

    minor GUI improvement

--- src/com/udav/findduplicate/Main.java
+++ src/com/udav/findduplicate/Main.java
@@ -7,11 +7,9 @@ public class Main {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		//MainFrame mf = new MainFrame();
-		//mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		MainFrame2 mf2 = new MainFrame2();
-		mf2.setVisible(true);
+	public static void main(String[] args) {		
+		MainFrame mf = new MainFrame();
+		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//new Finder();
 
 	}
